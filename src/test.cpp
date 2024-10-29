9bf54a3f2c23567b0c18c8240528bdd48fcb5a9e
ggonu <gogun0928@naver.com>
2024-10-30 01:31:17 +0900

    1st commit

---
+++ src/test.cpp
@@ -0,0 +1,11 @@
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_nsgs/msg/point_cloud2.hpp"
+
+/**
+ * TODO: build and install kf_lib
+ */
+// #include "ekf.hpp"
+
+class SensorFusionNode : public rclcpp::Node {
+
+}
